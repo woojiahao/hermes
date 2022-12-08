cb83f38bdb8c9e2b51ccfb882c8f48057ff68ece
Woo Jia Hao <woojiahao1234@gmail.com>
2022-12-08 20:13:31 +0800

    Style login popup

--- hermes-frontend/src/components/LoginPopup.tsx
+++ hermes-frontend/src/components/LoginPopup.tsx
@@ -2,8 +2,22 @@ import React from "react";
 
 export default function LoginPopup() {
   return (
-    <div>
-      <h1>Hello</h1>
+    <div className="login">
+      <h3 className="heading">Login to hermes</h3>
+      <form action="POST">
+        <div>
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" />
+        </div>
+        <div className="buttons">
+          <button>Register</button>
+          <button className="login-button">Login</button>
+        </div>
+      </form>
     </div>
   )
 }
