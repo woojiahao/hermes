55a3f8637f888973f248329c08d0317300ebc242
Woo Jia Hao <woojiahao1234@gmail.com>
2022-12-08 11:29:05 +0800

    Add tags on thread creation

--- hermes-backend/internal/database/thread.go
+++ hermes-backend/internal/database/thread.go
@@ -4,6 +4,8 @@ import (
 	"database/sql"
 	"errors"
 	"time"
+
+	i "woojiahao.com/hermes/internal"
 )
 
 type Tag struct {
@@ -12,7 +14,18 @@ type Tag struct {
 	HexCode string
 }
 
-// TODO: Add list of tags
+func NewTag(content, hexCode string) Tag {
+	return Tag{Content: content, HexCode: hexCode}
+}
+
+func parseTagRows(rows *sql.Rows) (Tag, error) {
+	var tag Tag
+	err := rows.Scan(&tag.Id, &tag.Content, &tag.HexCode)
+	return tag, err
+}
+
+// Opt to use two queries in a transaction to populate the Thread and its associated tags to avoid unnecessarily complex
+// queries or parsing
 type Thread struct {
 	Id          string
 	IsPublished bool
@@ -24,9 +37,10 @@ type Thread struct {
 	UpdatedAt   *time.Time
 	DeletedAt   *time.Time
 	DeletedBy   sql.NullString
+	Tags        []Tag
 }
 
-var dummyThread = Thread{"", false, false, "", "", nil, "", nil, nil, sql.NullString{}}
+var dummyThread = Thread{"", false, false, "", "", nil, "", nil, nil, sql.NullString{}, make([]Tag, 0)}
 
 func parseThreadRows(rows *sql.Rows) (Thread, error) {
 	var thread Thread
@@ -45,23 +59,86 @@ func parseThreadRows(rows *sql.Rows) (Thread, error) {
 	return thread, err
 }
 
-func (d *Database) CreateThread(userId, title, content string) (Thread, error) {
-	threads, err := query(
-		d,
-		`
-			INSERT INTO thread (title, "content", created_by)
-			VALUES ($1, $2, $3)
-			RETURNING *;
-		`,
-		generate_params(title, content, userId),
-		parseThreadRows,
-	)
-
-	if err != nil {
-		return dummyThread, err
+func (d *Database) CreateThread(userId, title, content string, tags []Tag) (Thread, error) {
+	var tag_contents []string
+	for _, tag := range tags {
+		tag_contents = append(tag_contents, tag.Content)
 	}
 
-	return threads[0], err
+	if i.HasDuplicates(tag_contents) {
+		return dummyThread, &i.ServerError{Custom: "tags cannot have same name", Base: nil}
+	}
+
+	return transaction(d, func(tx *sql.Tx) (Thread, error) {
+		threads, err := transactionQuery(
+			tx,
+			`
+				INSERT INTO thread (title, "content", created_by)
+				VALUES ($1, $2, $3)
+				RETURNING *;
+			`,
+			generate_params(title, content, userId),
+			parseThreadRows,
+		)
+		if err != nil {
+			return dummyThread, &i.DatabaseError{Custom: "failed to create new thread", Base: err}
+		}
+
+		thread := threads[0]
+
+		var db_tags []Tag
+
+		// Create or retrieve all of the tags from the database
+		// Once retrieved, attach the tag to the thread
+		for _, tag := range tags {
+			ts, err := transactionQuery(
+				tx,
+				`
+					WITH q AS(
+						INSERT INTO tag ("content", hex_code)
+						VALUES ($1, $2)
+						ON CONFLICT("content")
+						DO NOTHING
+						RETURNING *
+					)
+					SELECT * FROM q
+					UNION
+					SELECT * FROM tag WHERE tag."content" = $1;
+				`,
+				generate_params(tag.Content, tag.HexCode),
+				parseTagRows,
+			)
+
+			if err != nil {
+				return dummyThread, &i.DatabaseError{Custom: "failed to create/retrieve new tag", Base: err}
+			}
+
+			db_tags = append(db_tags, ts[0])
+
+			_, err = transactionQuery(
+				tx,
+				`
+					INSERT INTO thread_tag
+					VALUES ($1, $2)
+					RETURNING *;
+				`,
+				generate_params(thread.Id, ts[0].Id),
+				func(r *sql.Rows) (string, error) {
+					return "", nil // this perRow fn does not parse the results
+				},
+			)
+
+			if err != nil {
+				return dummyThread, &i.DatabaseError{Custom: "failed to link thread with tag", Base: err}
+			}
+		}
+
+		thread.Tags = db_tags
+
+		tx.Commit()
+
+		return thread, nil
+	})
 }
 
 func (d *Database) GetUserThreads(userId string) ([]Thread, error) {
