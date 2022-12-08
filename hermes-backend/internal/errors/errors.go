c2fb96fcffae8212a61d8112248ef8bc029e6cad
Woo Jia Hao <woojiahao1234@gmail.com>
2022-12-08 11:14:44 +0800

    Implement custom errors

---
+++ hermes-backend/internal/errors/errors.go
@@ -0,0 +1,23 @@
+package errors
+
+import "fmt"
+
+// Database error that occurs during query
+type DatabaseError struct {
+	base   error
+	custom string
+}
+
+func (dbe *DatabaseError) Error() string {
+	return fmt.Sprintf("database error occurred: %s, base: %s", dbe.custom, dbe.base.Error())
+}
+
+// Generic server error
+type ServerError struct {
+	base   error
+	custom string
+}
+
+func (se *ServerError) Error() string {
+	return fmt.Sprintf("generic server error occurred: %s, base: %s", se.custom, se.base.Error())
+}
