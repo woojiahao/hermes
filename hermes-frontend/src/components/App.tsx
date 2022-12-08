e0a23a9e2a9831b16f4b0a02338831e8c4a4a5e4
Woo Jia Hao <woojiahao1234@gmail.com>
2022-12-08 18:20:21 +0800

    Add React Router

--- hermes-frontend/src/components/App.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function App() {
-  return (
-    <div className="App">
-      <p>Hello world</p>
-    </div>
-  );
-}
--- hermes-frontend/src/index.tsx
+++ hermes-frontend/src/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './routes/app';
 import './index.css';
+import About from './routes/about';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "/about",
+    element: <About></About>,
+  },
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
